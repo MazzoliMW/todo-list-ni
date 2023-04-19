ddaf1960cb8295d7e12031b2a6758360cd8cc1a9
MazzoliMW <riccardo.mazzoli@marketwall.com>
2023-04-19 17:32:12 +0200

    TFF

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -38,43 +38,43 @@ class Todo extends Component{
      
   editingTemplate = () => (
     <form className="stack-small" onSubmit={this.handleSubmit}>
-      <div className="form-group">
-        <label className="todo-label" htmlFor={this.props.id}>
-          Nuovo nome per {this.props.name}
-        </label>
-        <input
-          id={this.props.id}
-          className="todo-text form-control"
-          type="text"
-          value={this.newName}
-          onChange={this.handleChange}
-          ref={this.editFieldRef}
-        />
-      </div>
-      <div className="btn-group">
-        <button
-          type="button"
-          className="btn todo-cancel btn-outline-warning btn-sm"
-          onClick={() => this.setState({ isEditing : false })}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
-            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-          </svg>
-          <span className="visually-hidden">rinomina {this.props.name}</span>
-        </button>
-        <button type="submit" className="btn btn__primary todo-edit btn-outline-primary btn-sm">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
-            <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
-          </svg>
-          <span className="visually-hidden">nuovo nome per {this.props.name}</span>
-        </button>
+          <label className="todo-label" htmlFor={this.props.id}>
+            Nuovo nome per {this.props.name}
+          </label>
+      <div class="input-group mb-3">
+          <button
+            type="button"
+            className="btn todo-cancel btn-outline-warning btn-sm"
+            onClick={() => this.setState({ isEditing : false })}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
+              <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+            </svg>
+            <span className="visually-hidden">rinomina {this.props.name}</span>
+          </button>
+          <input
+            id={this.props.id}
+            className="todo-text form-control"
+            type="text"
+            value={this.newName}
+            onChange={this.handleChange}
+            ref={this.editFieldRef}
+          />
+        <div className="btn-group">
+          <button type="submit" className="btn btn__primary todo-edit btn-outline-primary btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+              <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
+            </svg>
+            <span className="visually-hidden">nuovo nome per {this.props.name}</span>
+          </button>
+        </div>
       </div>
     </form>
   );
   viewTemplate = () => (
-      <div>
+      <div class="input-group mb-3">
         <div className="form-check form-switch">
             <input 
               className="form-check-input"
@@ -88,6 +88,7 @@ class Todo extends Component{
                 {this.props.name}
             </label>
         </div>
+        <div className="spacer2"></div>
         <div className="btn-group">
             <button
               type="button"
